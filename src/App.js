cd5d63a4eb3c1f29eaead3de6a7dbcef267a2a38
Rabina Karki <=>
2022-05-02 19:21:04 +0545

    dark mode complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+import { useState } from 'react';
 import './App.css';
 import About from './components/About';
+import Alert from './components/Alert';
 import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
+import TextForm from './components/TextForm';
 
 function App() {
+  const [mode, setMode] = useState('light'); //whether dark mode is enabled or not
+
+  const toggleMode = () =>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+    } 
+    else{
+      setMode('light');
+    }
+  }
   return (
 <>  
-<Navbar title="TextUtils" aboutText="About us"/>
+<Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
 <div className="container my-3" >
-  {/* <TextForm heading="Enter the text to analyze below"/> */}
+  <TextForm heading="Enter the text to analyze below" mode={mode}/>
   <About/>
+  <Alert alert="HI I am Alert Now !!"/>
 </div>
 </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -28,10 +28,10 @@ function TextForm(props) {
   return (
 
     <>
-    <div className="container">
+    <div className="container" style={{color: props.mode==='dark' ?'white':'#042743'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="10"></textarea>
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor :props.mode==='dark' ?'grey':'white' , color:props.mode === 'dark' ? 'white': '#042743'}} id="myBox" rows="10"></textarea>
             </div>
            <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
            <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to Lowercase</button>
@@ -39,12 +39,12 @@ function TextForm(props) {
 
     </div>
 
-    <div className="container my-3" >
+    <div className="container my-3" style={{color :props.mode==='dark' ?'white':'#042743'}} >
       <h2>Your text summary</h2>
       <p>{text.split(" ").length} word and {text.length} characters.</p>
       <p>{ 0.008 * text.split(" ").length} Minutes read</p>
       <h2>Preview</h2>
-      <p>{text}</p>
+      <p>{text.length > 0 ? text: "Enter something in the textbox above to preview it here"}</p>
     </div>
     </>
   )
