613895c50dabfa84492af310ca80b3d205ac8a5e
Rabina Karki <karkirabu@gmail.com>
2022-05-02 22:48:10 +0545

    minor update

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -10,7 +10,6 @@ const capitalize = (word) =>{
    
       props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
         <strong>{capitalize(props.alert.type)}</strong> : {props.alert.msg}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
       </div>
    
   )
